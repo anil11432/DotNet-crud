b105590b4ce80b08c645694a1f41f59ae2851328
anil kumar <anilk11432@gmail.com>
2017-12-18 19:59:59 +0530

    first commit

---
+++ default.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="default.aspx.cs" Inherits="_default" %>
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:HiddenField ID="hfproductID" runat="server" />
+        <table>
+            <tr>
+                <td>
+                     <asp:Label Text="PRODUCT" runat="server"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtproduct" runat="server"></asp:TextBox>
+
+                </td>
+            </tr>
+             <tr>
+                <td>
+                     <asp:Label Text="PRICE" runat="server"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtprice" runat="server" TextMode="Number"></asp:TextBox>
+
+                </td>
+            </tr>
+             <tr>
+                <td>
+                     <asp:Label Text="COUNT" runat="server"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtcount" runat="server" TextMode="Number"></asp:TextBox>
+
+                </td>
+            </tr>
+             <tr>
+                <td>
+                     <asp:Label Text="DESCRIPTION" runat="server"></asp:Label>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtdecription" runat="server"></asp:TextBox>
+
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="4">
+                    <asp:Button ID="btninsert" runat="server" Text="INSERT" OnClick="btninsert_Click" />
+                    <asp:Button ID="btndelete" runat="server" Text="DELETE" OnClick="btndelete_Click" />
+                    <asp:Button ID="btnclear" runat="server" Text="CLEAR" OnClick="btnclear_Click" />
+                    <asp:Button ID="btnupdate" runat="server" Text="UPDATE" OnClick="btnupdate_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2">
+                    <asp:Label ID="succes" runat="server" Text=""></asp:Label>
+                    <asp:Label ID="error" runat="server" Text=""></asp:Label>
+                </td>
+            </tr>
+        </table>
+        <asp:GridView ID="grd1" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="product" HeaderText="product" />
+                <asp:BoundField DataField="price" HeaderText="product" />
+                <asp:BoundField DataField="count" HeaderText="product" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton Text="edit" ID="lnkedit" CommandArgument='<%# Eval("productid") %>' runat="server" OnClick="lnk_edit"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+            </Columns>
+
+        </asp:GridView>
+       
+     </div>
+    </form>
+    <script>
+        function(){
+            document.getElementById("form1").preventDefault();
+        }
+    </script>
+</body>
+</html>
---
+++ default.aspx.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Data;
+
+public partial class _default : System.Web.UI.Page
+{
+    string cs = ConfigurationManager.ConnectionStrings["crudCon"].ToString();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            clear();
+            gridFill();
+        }
+    }
+
+    protected void btninsert_Click(object sender, EventArgs e)
+    {
+        
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("AddOrEDIT", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@productid", (hfproductID.Value == "" ? 0 : Convert.ToInt32(hfproductID.Value)));
+                    cmd.Parameters.AddWithValue("@product", txtproduct.Text.Trim());
+                    cmd.Parameters.AddWithValue("@price", Convert.ToDecimal(txtprice.Text.Trim()));
+                    cmd.Parameters.AddWithValue("@count", Convert.ToInt32(txtcount.Text.Trim()));
+                    cmd.Parameters.AddWithValue("@description", txtdecription.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    gridFill();
+                    clear();
+                    succes.Text = "SAVED SUCCESFULLY!";
+                }
+                catch (Exception ex)
+                {
+
+                    error.Text = ex.Message;
+                }
+               
+                
+
+
+
+            }
+
+        }
+       
+    
+    protected void btndelete_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("deleteById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@productid", (hfproductID.Value==""?0:Convert.ToInt32(hfproductID.Value)));
+           
+            cmd.ExecuteNonQuery();
+            gridFill();
+            clear();
+            succes.Text = "DELETED SUCCESFULLY!";
+
+
+
+
+        }
+    }
+    protected void btnclear_Click(object sender, EventArgs e)
+    {
+        clear();
+    }
+    protected void btnupdate_Click(object sender, EventArgs e)
+    {
+
+        try
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Edit", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@productid", (hfproductID.Value == "" ? 0 : Convert.ToInt32(hfproductID.Value)));
+                cmd.Parameters.AddWithValue("@product", txtproduct.Text.Trim());
+                cmd.Parameters.AddWithValue("@price", Convert.ToDecimal(txtprice.Text.Trim()));
+                cmd.Parameters.AddWithValue("@count", Convert.ToInt32(txtcount.Text.Trim()));
+                cmd.Parameters.AddWithValue("@description", txtdecription.Text.Trim());
+                cmd.ExecuteNonQuery();
+                gridFill();
+                clear();
+                succes.Text = "Edited SUCCESFULLY!";
+
+
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+            error.Text = ex.Message;
+        }
+    }
+    protected void lnk_edit(object sender, EventArgs e)
+    {
+        int productid = Convert.ToInt32((sender as LinkButton).CommandArgument);
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("ViewById", con);
+            da.SelectCommand.Parameters.AddWithValue("@productid", productid);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            txtproduct.Text = dt.Rows[0][1].ToString();
+            txtprice.Text = dt.Rows[0][2].ToString();
+            txtcount.Text = dt.Rows[0][3].ToString();
+            txtdecription.Text = dt.Rows[0][4].ToString();
+            hfproductID.Value = dt.Rows[0][0].ToString();
+            btndelete.Enabled = true;
+        }
+    }
+    public void clear()
+    {
+        hfproductID.Value = "";
+        txtproduct.Text = txtprice.Text = txtcount.Text = txtdecription.Text = "";
+        btndelete.Enabled = false;
+        succes.Text = error.Text = "";
+    }
+     void gridFill()
+    {
+        using(SqlConnection con=new SqlConnection(cs))
+        {
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("viewAll", con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            grd1.DataSource = ds;
+            grd1.DataBind();
+        }
+    }
+}
+	
+
+
+
